4cf041b65fc17ae0fb77f1938e9324c5e89ee813
Connor Hubbard <connor@dominantstrategies.io>
2023-06-16 15:43:44 -0500

    cleaned up config

--- spammer/main.go
+++ spammer/main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"github.com/dominant-strategies/go-quai/common"
+	"github.com/dominant-strategies/go-quai/core"
+	"github.com/dominant-strategies/go-quai/core/types"
+	"github.com/dominant-strategies/go-quai/crypto"
+	"io"
 	"math"
 	"math/big"
 	random "math/rand"
 	"os"
 	"time"
 
-	"github.com/dominant-strategies/go-quai/common"
-	"github.com/dominant-strategies/go-quai/core"
-	"github.com/dominant-strategies/go-quai/core/types"
-	"github.com/dominant-strategies/go-quai/crypto"
 	"github.com/dominant-strategies/go-quai/params"
 	"github.com/dominant-strategies/go-quai/quaiclient/ethclient"
 	accounts "github.com/dominant-strategies/quai-accounts"
@@ -53,43 +52,31 @@ func main() {
 		return
 	}
 	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := io.ReadAll(jsonFile)
 	var result map[string]map[string][]wallet
 	err = json.Unmarshal(byteValue, &result)
 	if err != nil {
-		fmt.Println(err)
+		panic(fmt.Errorf("error parsing wallets.json: %v", err))
 		return
 	}
 	SpamTxs(result, group)
 	<-exit
 }
 
-type AddressCache struct {
-	addresses [][]chan common.Address
-	privKeys  [][]chan ecdsa.PrivateKey
-}
-
 func SpamTxs(wallets map[string]map[string][]wallet, group string) {
 	config, err := util.LoadConfig(".")
+	fmt.Println("config loaded", config)
 	if err != nil {
 		fmt.Println("cannot load config: " + err.Error())
 		return
 	}
 	allClients := getNodeClients(config)
-	region := -1
-	for i := 0; i < NUMZONES; i++ {
-		from_zone := i % 3
-		if i%3 == 0 {
-			region++
-		}
-
-		go func(from_zone int, region int) {
-			if !allClients.zonesAvailable[region][from_zone] {
-				return
-			}
-			client := allClients.zoneClients[region][from_zone]
-			signer := types.LatestSigner(PARAMS)
-			zoneWallets := wallets["group-"+group]["zone-"+fmt.Sprintf("%d-%d", region, from_zone)]
+	for zone, client := range allClients.zoneClients {
+		go func(zone string, client *ethclient.Client) {
+			signer := types.LatestSigner(
+				&params.ChainConfig{ChainID: big.NewInt(config.ChainId)},
+			)
+			zoneWallets := wallets["group-"+group][zone]
 			walletIndex := 0
 			walletsPerBlock := WALLETSPERBLOCK
 			txsSent := 0
@@ -133,7 +120,7 @@ func SpamTxs(wallets map[string]map[string][]wallet, group string) {
 				var toAddr common.Address
 				var tx *types.Transaction
 				toAddr = common.HexToAddress(zoneWallets[len(zoneWallets)-1-walletIndex].Address)
-				inner_tx := types.InternalTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: MAXFEE, Gas: GAS, To: &toAddr, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
+				inner_tx := types.InternalTx{ChainID: big.NewInt(config.ChainId), Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: MAXFEE, Gas: GAS, To: &toAddr, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
 				tx = types.NewTx(&inner_tx)
 
 				tx, err = types.SignTx(tx, signer, fromPrivKey)
@@ -143,9 +130,9 @@ func SpamTxs(wallets map[string]map[string][]wallet, group string) {
 				}
 				err = client.SendTransaction(context.Background(), tx)
 				if err != nil {
-					fmt.Printf("zone-" + fmt.Sprintf("%d-%d", region, from_zone) + ": " + err.Error() + "\n")
+					fmt.Printf(zone + ": " + err.Error() + "\n")
 					if err.Error() == core.ErrReplaceUnderpriced.Error() {
-						inner_tx := types.InternalTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: new(big.Int).Mul(big.NewInt(2), MINERTIP), GasFeeCap: new(big.Int).Mul(big.NewInt(2), MAXFEE), Gas: GAS, To: &toAddr, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
+						inner_tx := types.InternalTx{ChainID: big.NewInt(config.ChainId), Nonce: nonce, GasTipCap: new(big.Int).Mul(big.NewInt(2), MINERTIP), GasFeeCap: new(big.Int).Mul(big.NewInt(2), MAXFEE), Gas: GAS, To: &toAddr, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
 						tx = types.NewTx(&inner_tx)
 						tx, err = types.SignTx(tx, signer, fromPrivKey)
 						if err != nil {
@@ -195,13 +182,13 @@ func SpamTxs(wallets map[string]map[string][]wallet, group string) {
 				if txsSent%walletsPerBlock == 0 && walletIndex != 0 { // not perfect math in the case that walletIndex wraps around to zero
 					elapsed := time.Since(start)
 					tps := float64(walletsPerBlock) / elapsed.Seconds()
-					fmt.Printf("zone-"+fmt.Sprintf("%d-%d", region, from_zone)+": Time elapsed for %d txs: %d ms TPS: %f Txs Sent: %d ", walletsPerBlock, elapsed.Milliseconds(), tps, txsSent)
+					fmt.Printf(zone+": Time elapsed for %d txs: %d ms TPS: %f Txs Sent: %d ", walletsPerBlock, elapsed.Milliseconds(), tps, txsSent)
 
 					tpsInNS := float64(walletsPerBlock) / float64(elapsed.Seconds())
 					newSleepBasedOnCalcTPS := float64(sleepPerTx.Nanoseconds()) + (float64(sleepPerTx.Nanoseconds()) * float64(tpsInNS-float64(targetTPS)) * 0.01) // newSleep = oldSleep * (tps / targetTPS)
 					fmt.Println("controller:", "old", sleepPerTx.Seconds(), "Error", float64(tpsInNS-float64(targetTPS)), "new", newSleepBasedOnCalcTPS)
 					sleepPerTx = time.Duration(math.Max(newSleepBasedOnCalcTPS, 0))
-					fmt.Printf("zone-"+fmt.Sprintf("%d-%d", region, from_zone)+": New Sleep: %d ms\n", sleepPerTx.Milliseconds())
+					fmt.Printf(zone+": New Sleep: %d ms\n", sleepPerTx.Milliseconds())
 					start = time.Now()
 
 					if tps > float64(targetTPS) {
@@ -216,7 +203,7 @@ func SpamTxs(wallets map[string]map[string][]wallet, group string) {
 				}
 
 			}
-		}(from_zone, region)
+		}(zone, client)
 	}
 }
 
@@ -226,7 +213,7 @@ type orderedBlockClients struct {
 	primeAvailable   bool
 	regionClients    []*ethclient.Client
 	regionsAvailable []bool
-	zoneClients      [][]*ethclient.Client
+	zoneClients      map[string]*ethclient.Client
 	zonesAvailable   [][]bool
 	zoneAccounts     [][]accounts.Account
 	zoneWallets      [][]wallet
@@ -239,98 +226,24 @@ func getNodeClients(config util.Config) orderedBlockClients {
 
 	// initializing all the clients
 	allClients := orderedBlockClients{
-		primeAvailable:   false,
-		regionClients:    make([]*ethclient.Client, 3),
-		regionsAvailable: make([]bool, 3),
-		zoneClients:      make([][]*ethclient.Client, 3),
-		zonesAvailable:   make([][]bool, 3),
-		zoneAccounts:     make([][]accounts.Account, 3),
+		zoneClients:    make(map[string]*ethclient.Client, 3),
+		zonesAvailable: make([][]bool, 3),
+		zoneAccounts:   make([][]accounts.Account, 3),
 	}
 
-	for i := range allClients.zoneClients {
-		allClients.zoneClients[i] = make([]*ethclient.Client, 3)
-	}
 	for i := range allClients.zonesAvailable {
 		allClients.zonesAvailable[i] = make([]bool, 3)
 	}
-	for i := range allClients.zoneClients {
-		allClients.zoneAccounts[i] = make([]accounts.Account, 3)
-	}
+	//for i := range allClients.zoneClients {
+	//	allClients.zoneAccounts[i] = make([]accounts.Account, 3)
+	//}
 
-	// add Prime to orderedBlockClient array at [0]
-	if config.PrimeURL != "" {
-		primeClient, err := ethclient.Dial(config.PrimeURL)
+	for zone, ports := range config.Ports {
+		zoneClient, err := ethclient.Dial(fmt.Sprintf("ws://localhost:%d", ports.Ws))
 		if err != nil {
-			fmt.Println("Unable to connect to node:", "Prime", config.PrimeURL)
-		} else {
-			allClients.primeClient = primeClient
-			allClients.primeAvailable = true
+			panic(fmt.Errorf("could not connect to zone %s: %v", zone, err))
 		}
-	}
-
-	// loop to add Regions to orderedBlockClient
-	// remember to set true value for Region to be mined
-	for i, regionURL := range config.RegionURLs {
-		if regionURL != "" {
-			regionClient, err := ethclient.Dial(regionURL)
-			if err != nil {
-				fmt.Println("Unable to connect to node:", "Region", i+1, regionURL)
-				allClients.regionsAvailable[i] = false
-			} else {
-				allClients.regionsAvailable[i] = true
-				allClients.regionClients[i] = regionClient
-			}
-		}
-	}
-
-	// loop to add Zones to orderedBlockClient
-	// remember ZoneURLS is a 2D array
-	for i, zonesURLs := range config.ZoneURLs {
-		for j, zoneURL := range zonesURLs {
-			if zoneURL != "" {
-				zoneClient, err := ethclient.Dial(zoneURL)
-				if err != nil {
-					fmt.Println("Unable to connect to node:", "Zone", i+1, j+1, zoneURL)
-					allClients.zonesAvailable[i][j] = false
-				} else {
-					allClients.zonesAvailable[i][j] = true
-					allClients.zoneClients[i][j] = zoneClient
-				}
-			}
-		}
+		allClients.zoneClients[zone] = zoneClient
 	}
 	return allClients
-}
-
-func Location(a common.Address) *common.Location {
-
-	// Search zone->region->prime address spaces in-slice first, and then search
-	// zone->region out-of-slice address spaces next. This minimizes expected
-	// search time under the following assumptions:
-	// * a node is more likely to encounter a TX from its slice than from another
-	// * we expect `>= Z` `zone` TXs for every `region` TX
-	// * we expect `>= R` `region` TXs for every `prime` TX
-	// * (and by extension) we expect `>= R*Z` `zone` TXs for every `prime` TX
-	primeChecked := false
-	for r := 0; r < common.NumRegionsInPrime; r++ {
-		for z := 0; z < common.NumZonesInRegion; z++ {
-			l := common.Location{byte(r), byte(z)}
-			if l.ContainsAddress(a) {
-				return &l
-			}
-		}
-		l := common.Location{byte(r)}
-		if l.ContainsAddress(a) {
-			return &l
-		}
-		// Check prime on first pass through slice, but not again
-		if !primeChecked {
-			primeChecked = true
-			l := common.Location{}
-			if l.ContainsAddress(a) {
-				return &l
-			}
-		}
-	}
-	return nil
 }
--- util/config.go
+++ util/config.go
@@ -1,29 +1,106 @@
 package util
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"strconv"
 
-	"github.com/dominant-strategies/go-quai/common"
 	"github.com/spf13/viper"
 )
 
+type JsonRequest struct {
+	JsonRPC string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	Id      int    `json:"id"`
+}
+
+type JsonResponse struct {
+	JsonRPC string `json:"jsonrpc"`
+	Id      int    `json:"id"`
+	Result  string `json:"result"`
+}
+
+type ResponseData struct {
+	Error  string `json:"error"`
+	Result string `json:"result"`
+}
+
+func lookupChainId(url string) (int64, error) {
+	jsonData := JsonRequest{
+		JsonRPC: "2.0",
+		Method:  "eth_chainId",
+		Id:      1,
+	}
+
+	jsonValue, _ := json.Marshal(jsonData)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+
+	var jsonResponse JsonResponse
+
+	json.Unmarshal(body, &jsonResponse)
+
+	chainId, err := strconv.ParseInt(jsonResponse.Result[2:], 16, 64) // omit the "0x" prefix
+	if err != nil {
+		return 0, err
+	}
+
+	return chainId, nil
+}
+
 type Config struct {
-	PrimeURL      string
-	RegionURLs    []string
-	ZoneURLs      [][]string
-	Location      common.Location
-	Auto          bool
-	Optimize      bool
-	OptimizeTimer int
+	Ports   map[string]Zone
+	ChainId int64
+}
+
+type Zone struct {
+	Http int `mapstructure:"http"`
+	Ws   int `mapstructure:"ws"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
 func LoadConfig(path string) (config Config, err error) {
+	networks := map[int64]string{
+		1337:  "default",
+		9000:  "colosseum",
+		12000: "garden",
+		15000: "orchard",
+		17000: "galena",
+	}
+
 	viper.AddConfigPath("./config")
-	viper.SetConfigName("config") // name of config file (without extension)
-	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(".")      // optionally look for config in the working directory
-	err = viper.ReadInConfig()    // Find and read the config file
+	viper.SetConfigName("default") // name of config file (without extension)
+	viper.SetConfigType("yaml")    // REQUIRED if the config file does not have the extension in the name
+	err = viper.ReadInConfig()     // Find and read the config file
+
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+
+	chainId, err := lookupChainId(fmt.Sprintf("http://localhost:%d", config.Ports["zone-0-0"].Http)) // add host as an input to this fn that comes from a flag
+	if err != nil {                                                                                  // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error getting chainId: %w \n", err))
+	}
+	config.ChainId = chainId
+
+	viper.SetConfigName(networks[chainId]) // name of config file (without extension)
+	err = viper.MergeInConfig()            // Find and read the config file
 
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %w \n", err))
